739405eec308626fcec50c8672796c2fdbe8d119
siiriinno <siiri.inno@tlu.ee>
2022-06-09 15:33:09 +0200

    Parandus 5

--- function.php
+++ function.php
@@ -17,6 +17,6 @@ if (!function_exists('post_setup')) :
 endif;
 add_action('after_setup_theme', 'post_setup');
 function my_style_enqueue_styles() {
-    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css');
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/general.css');
 }
 add_action('wp_enqueue_scripts', 'my_style_enqueue_styles');
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="description" content="Veebiprogrammeeerimise eksam">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body>
